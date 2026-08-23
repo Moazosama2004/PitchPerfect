76235ba8556d1d19504030b958207cbae0813a9e
Moaz Osama <moazosama204@gmail.com>
2026-08-23 04:22:52 -0700

    AutoLayout and Buttons

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func recordAudio(_ sender: UIButton) {
+        print("Start Recording....")
+        recordingLabel.text = "Recording...."
+    }
+    
+    @IBAction func stopRecording(_ sender: UIButton) {
+        print("Stop Recording....")
+        recordingLabel.text = "Stop Recording...."
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in 
+            guard let self = self else {return}
+            self.recordingLabel.text = "Tap To Record"
+        }
+        
+    }
 }
 
