7739ef1c98cfedbff6eb0002871e401431bcf102
Moaz Osama <moazosama204@gmail.com>
2026-08-23 19:07:37 -0700

    ViewControllers and Multiple Views

--- PitchPerfect/Controller/ViewController.swift
+++ PitchPerfect/Controller/ViewController.swift
@@ -10,24 +10,34 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+    
+        stopRecordingButton.isEnabled = false
+  }
+    
+    override func viewWillAppear(_ animated: Bool) {
+  }
 
 
     @IBAction func recordAudio(_ sender: UIButton) {
-        print("Start Recording....")
         recordingLabel.text = "Recording...."
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
     
     @IBAction func stopRecording(_ sender: UIButton) {
         print("Stop Recording....")
         recordingLabel.text = "Stop Recording...."
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in 
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
             guard let self = self else {return}
             self.recordingLabel.text = "Tap To Record"
+            self.recordButton.isEnabled = true;
+            stopRecordingButton.isEnabled = false
         }
         
     }
